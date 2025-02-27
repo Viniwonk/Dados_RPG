ad8a5ba4738128d7f6c05e72d5d46e12fa3014b5
Viniwonk <vicimon15@gmail.com>
2025-02-27 19:59:11 -0300

    git pus

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ const tab = document.querySelectorAll(".tab");
 const dicePNG = document.querySelector(".dado");
 const historico = document.getElementById("historico");
 
-// Dados
-const dados = {
-  D4: () => Math.trunc(Math.random() * 4) + 1,
-  D6: () => Math.trunc(Math.random() * 6) + 1,
-  D8: () => Math.trunc(Math.random() * 8) + 1,
-  D10: () => Math.trunc(Math.random() * 10) + 1,
-  D12: () => Math.trunc(Math.random() * 12) + 1,
-  D20: () => Math.trunc(Math.random() * 20) + 1,
-};
-
-//Armazenamento dos Dados
-let rolagens = [];
-
 //Aba Ativa
 function abaAtiva(button) {
   tab.forEach(function (btn) {
@@ -30,6 +17,19 @@ tab.forEach(function (button) {
     abaAtiva(button);
   });
 });
+
+// Dados
+const dados = {
+  D4: () => Math.trunc(Math.random() * 4) + 1,
+  D6: () => Math.trunc(Math.random() * 6) + 1,
+  D8: () => Math.trunc(Math.random() * 8) + 1,
+  D10: () => Math.trunc(Math.random() * 10) + 1,
+  D12: () => Math.trunc(Math.random() * 12) + 1,
+  D20: () => Math.trunc(Math.random() * 20) + 1,
+};
+
+//Armazenamento dos Dados
+let rolagens = [];
 
 //Hidden
 dicePNG.classList.add("hidden");
@@ -53,7 +53,6 @@ rolaDado.addEventListener("click", function () {
 });
 
 //Histórico de Rolagens
-
 function mostrarRolagens() {
   historico.innerHTML = rolagens.join("<br>");
 }
